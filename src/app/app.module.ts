b1290dce4b1899bfbe194f7f01225c32638ac974
killua <killualolxx@gmail.com>
2023-06-05 20:14:39 -0300

    components criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ExpensesComponent } from './componentes/expenses/expenses.component';
+import { IncomeComponent } from './components/income/income.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ExpensesComponent,
+    IncomeComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
